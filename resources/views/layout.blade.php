f672379ef4c7ad63c3d0f76d416f5d377cd6d2b2
Unknown <carlosluquegutierrez@gmail.com>
2017-12-23 19:43:05 +0100

    1ª Separación de vistas

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,23 +8,7 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
-  <meta name="author" content="Łukasz Holeczek">
-  <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,AngularJS,Angular,Angular2,Angular 2,Angular4,Angular 4,jQuery,CSS,HTML,RWD,Dashboard,React,React.js,Vue,Vue.js">
-  <link rel="shortcut icon" href="img/favicon.png">
-  <title>CoreUI - Open Source Bootstrap Admin Template</title>
-
-  <!-- Icons -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css" >
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" rel="stylesheet" type="text/css" >
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-
-  <!-- Main styles for this application -->
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  @yield('head')
 </head>
 
 <!-- BODY options, add following classes to body to change options
@@ -247,101 +231,7 @@
 
       <!-- Tab panes -->
       <div class="tab-content">
-        <div class="tab-pane active" id="timeline" role="tabpanel">
-          <div class="callout m-0 py-2 text-muted text-center bg-light text-uppercase">
-            <small><b>Today</b></small>
-          </div>
-          <hr class="transparent mx-3 my-0">
-          <div class="callout callout-warning m-0 py-3">
-            <div class="avatar float-right">
-              <img src="img/avatars/7.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-            </div>
-            <div>Meeting with
-              <strong>Lucas</strong>
-            </div>
-            <small class="text-muted mr-3"><i class="icon-calendar"></i>&nbsp; 1 - 3pm</small>
-            <small class="text-muted"><i class="icon-location-pin"></i>&nbsp; Palo Alto, CA </small>
-          </div>
-          <hr class="mx-3 my-0">
-          <div class="callout callout-info m-0 py-3">
-            <div class="avatar float-right">
-              <img src="img/avatars/4.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-            </div>
-            <div>Skype with
-              <strong>Megan</strong>
-            </div>
-            <small class="text-muted mr-3"><i class="icon-calendar"></i>&nbsp; 4 - 5pm</small>
-            <small class="text-muted"><i class="icon-social-skype"></i>&nbsp; On-line </small>
-          </div>
-          <hr class="transparent mx-3 my-0">
-          <div class="callout m-0 py-2 text-muted text-center bg-light text-uppercase">
-            <small><b>Tomorrow</b></small>
-          </div>
-          <hr class="transparent mx-3 my-0">
-          <div class="callout callout-danger m-0 py-3">
-            <div>New UI Project -
-              <strong>deadline</strong>
-            </div>
-            <small class="text-muted mr-3"><i class="icon-calendar"></i>&nbsp; 10 - 11pm</small>
-            <small class="text-muted"><i class="icon-home"></i>&nbsp; creativeLabs HQ </small>
-            <div class="avatars-stack mt-2">
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/2.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/3.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/4.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/5.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-            </div>
-          </div>
-          <hr class="mx-3 my-0">
-          <div class="callout callout-success m-0 py-3">
-            <div>
-              <strong>#10 Startups.Garden</strong> Meetup</div>
-            <small class="text-muted mr-3"><i class="icon-calendar"></i>&nbsp; 1 - 3pm</small>
-            <small class="text-muted"><i class="icon-location-pin"></i>&nbsp; Palo Alto, CA </small>
-          </div>
-          <hr class="mx-3 my-0">
-          <div class="callout callout-primary m-0 py-3">
-            <div>
-              <strong>Team meeting</strong>
-            </div>
-            <small class="text-muted mr-3"><i class="icon-calendar"></i>&nbsp; 4 - 6pm</small>
-            <small class="text-muted"><i class="icon-home"></i>&nbsp; creativeLabs HQ </small>
-            <div class="avatars-stack mt-2">
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/2.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/3.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/4.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/5.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/7.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-              <div class="avatar avatar-xs">
-                <img src="img/avatars/8.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-              </div>
-            </div>
-          </div>
-          <hr class="mx-3 my-0">
-        </div>
+        @yield('right_panel')
         <div class="tab-pane p-3" id="messages" role="tabpanel">
           <div class="message">
             <div class="py-3 pb-5 mr-3 float-left">
@@ -516,20 +406,7 @@
     <span class="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>
   </footer>
 
-<!-- Bootstrap and necessary plugins -->
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.3/js/tether.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
-
-<!-- Plugins and scripts required by all views -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2"></script>
- -->
-<!-- GenesisUI main scripts -->
-<script src="js/app.js"></script>
+@yield('javascript')
 
 </body>
 </html>
